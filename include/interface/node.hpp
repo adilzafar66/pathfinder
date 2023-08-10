286ef6e9e2784631ff91e8a5b37b7c69ee6d2c73
Adil <adilzafar66@gmail.com>
2023-08-09 23:55:59 -0700

    Add code for node from local repo

---
+++ include/interface/node.hpp
@@ -0,0 +1,45 @@
+#include <QGraphicsEllipseItem>
+#include <QGraphicsTextItem>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QPointF>
+
+namespace interface
+{
+    class ClickableVertexItem : public QGraphicsEllipseItem
+    {
+    public:
+        ClickableVertexItem(unsigned int position, Qt::GlobalColor color = Qt::red, QGraphicsItem *parent = nullptr) : QGraphicsEllipseItem(parent), vertex_pos(position), label(nullptr)
+        {
+            setRect(-8, -8, 16, 16);
+            setBrush(color);
+        }
+
+    protected:
+        void mousePressEvent(QGraphicsSceneMouseEvent *event) override
+        {
+            QGraphicsEllipseItem::mousePressEvent(event);
+            toggle_label();
+        }
+
+    private:
+        void toggle_label()
+        {
+            if (!label)
+            {
+                QPointF point = this->pos();
+                label = scene()->addText(QString::number(vertex_pos));
+                label->setPos(point);
+            }
+            else
+            {
+                scene()->removeItem(label);
+                delete label;
+                label = nullptr;
+            }
+        }
+
+        unsigned int vertex_pos;
+        QGraphicsTextItem *label;
+    };
+} // namespace interface
