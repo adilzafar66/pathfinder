13b580484f223c6bbd1e9d2f7f24d96a5ec5c39b
Adil <adilzafar66@gmail.com>
2023-08-09 23:31:03 -0700

    Add code for astar algorithm from local repo

---
+++ include/algorithm/astar.hpp
@@ -0,0 +1,83 @@
+#ifndef ASTAR_H
+#define ASTAR_H
+
+#include <queue>
+#include "../graph/graph.hpp"
+
+using namespace graph;
+
+namespace algorithm {
+
+std::vector<unsigned int> reconstruct_a_star_path(const std::unordered_map<unsigned int, unsigned int>& came_from, unsigned int current_position);
+
+template <class T>
+void compute_astar(Graph<T>& graph_, unsigned int start_position, unsigned int goal_position) {
+    // Initialize data structures for A* algorithm
+    using DistPos = std::pair<double, unsigned int>;
+    std::unordered_map<unsigned int, double> g_score; // Cost from start to each vertex
+    std::unordered_map<unsigned int, double> f_score; // Estimated total cost from start to goal through each vertex
+    std::unordered_map<unsigned int, unsigned int> came_from; // Previous vertex in the optimal path
+    std::priority_queue<DistPos, std::vector<DistPos>, std::greater<DistPos>> open_set;
+
+    // Initialize scores to infinity for all vertices except the start vertex
+    for (const auto& vertex_entry : graph_.get_vertices())
+    {
+        unsigned int vertex_position = vertex_entry.first;
+        g_score[vertex_position] = (vertex_position == start_position) ? 0 : std::numeric_limits<double>::infinity();
+        f_score[vertex_position] = (vertex_position == start_position) ? graph_.get_heuristic(start_position, goal_position) : std::numeric_limits<double>::infinity();
+        open_set.push(std::make_pair(f_score[vertex_position], vertex_position));
+    }
+
+    // A* algorithm
+    while (!open_set.empty())
+    {
+        unsigned int current_position = open_set.top().second;
+        open_set.pop();
+
+        if (current_position == goal_position) {
+            auto optimal_path = reconstruct_a_star_path(came_from, current_position);
+            optimal_path.insert(optimal_path.begin(), start_position);
+            graph_.set_astar_path(optimal_path);
+            return;   
+        }
+
+        const auto current_vertex = graph_.get_vertex(current_position); // Use your method to get the vertex
+
+        for (const auto& edge : current_vertex->get_edges())
+        {
+            unsigned int neighbor_position = edge->get_destination()->get_position();
+            double edge_weight = edge->get_cost();
+
+            if (edge_weight == -1) continue; // Skip untraversable edges
+
+            double tentative_g_score = g_score[current_position] + edge_weight;
+
+            if (tentative_g_score < g_score[neighbor_position])
+            {
+                came_from[neighbor_position] = current_position;
+                g_score[neighbor_position] = tentative_g_score;
+                f_score[neighbor_position] = g_score[neighbor_position] + graph_.get_heuristic(neighbor_position, goal_position);
+
+                // Update the priority queue with the new f_score
+                open_set.push(std::make_pair(f_score[neighbor_position], neighbor_position));
+            }
+        }
+    }
+    // No path found
+    graph_.set_astar_path(std::vector<unsigned int>());
+}
+
+std::vector<unsigned int> reconstruct_a_star_path(const std::unordered_map<unsigned int, unsigned int>& came_from, unsigned int current_position)
+{
+    std::vector<unsigned int> path;
+    while (came_from.find(current_position) != came_from.end())
+    {
+        path.push_back(current_position);
+        current_position = came_from.at(current_position);
+    }
+    std::reverse(path.begin(), path.end());
+    return path;
+}
+}
+
+#endif // ASTAR_H
